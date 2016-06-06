9655fa91e1891faeb0bc2a528761c26dfbaabdd2
Edgar Ribeiro <edgar.marques.ribeiro@gmail.com>
2016-06-06 17:55:13 +0100

    improve mail response

--- conf/tests/cn-hyper-test/test_ping.js
+++ conf/tests/cn-hyper-test/test_ping.js
@@ -8,14 +8,26 @@ console.log('payload', payload);
 console.log('server'. serverAddress);
   this.name = payload.repository.name;
   this.server = supertest.agent(serverAddress.Server);
+
+this.payload = payload;
+this.serverData = serverAddress;
 }
 
 Test.prototype.run = function (callback) {
-
+var that = this;
   this.server.get("/ping")
               .expect("Content-type",/json/)
               .expect(201)
-              .end(callback);
+              .end(function(err, result) {
+
+var data = {}
+   if(err) data.error = err.messsage
+
+data.result = result
+data.commit = that.payload
+data.config = that.serverData
+callback(null, data)
+});
 };
 
 module.exports = Test;
