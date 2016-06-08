5823c64f615257a890685597a2ea47889a33b51b
Edgar Ribeiro <edgar.marques.ribeiro@gmail.com>
2016-06-08 12:10:55 +0100

    add helper moment

--- bin/render.js
+++ bin/render.js
@@ -1,15 +1,33 @@
 const handlebars = require('handlebars');
 
 var app = require('express')(),
-bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-
+  const htmlToText = require('html-to-text');
+const helperMomentTimezone = require('./helper-moment-timezone')();
+const juice = require('juice');
+handlebars.registerHelper('moment', helperMomentTimezone.moment);
+handlebars.registerHelper('duration', helperMomentTimezone.duration);
+const fs = require('fs')
+const path = require('path')
 app.get('/render', function(req, res) {
-  const source = require('fs').readFileSync(require('path').join(__dirname, '../views/mail.hbs'), 'utf8');
+  const source = fs.readFileSync(path.join(__dirname,
+    '../views/mail.hbs'), 'utf8');
+  const css = fs.readFileSync(path.join(__dirname,
+    '../views/mail.css'), 'utf8');
   const template = handlebars.compile(source);
-  const context = require('../views/context_example.json');
-  res.end(template(context));
+  const context = JSON.parse(fs.readFileSync(path.join(__dirname,
+    '../views/context_example.json'), 'utf8'));
+  res.end(juice.inlineContent(template(context), css));
+});
+
+app.get('/text', function(req, res) {
+  const source = fs.readFileSync(path.join(__dirname,
+    '../views/mail.hbs'), 'utf8');
+  const template = handlebars.compile(source);
+  const context = JSON.parse(fs.readFileSync(path.join(__dirname,
+    '../views/context_example.json'), 'utf8'));
+  const css = fs.readFileSync(path.join(__dirname,
+    '../views/mail.css'), 'utf8');
+  res.end(htmlToText.fromString(juice.inlineContent(template(context), css)));
 });
 
 app.listen(8800, function() {
